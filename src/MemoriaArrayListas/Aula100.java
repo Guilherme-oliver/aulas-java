ea3b1e6352f1034f86a6264686093dc91e70dcbb
Perkles <guilherme.rodriguess.oliveira@gmail.com>
2021-11-30 19:33:18 -0300

    Atualização das aulas em Java

--- src/MemoriaArrayListas/Aula100.java
+++ src/MemoriaArrayListas/Aula100.java
@@ -6,32 +6,43 @@ public class Aula100 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite quantas linhas a matriz vai possuir: ");
-        int n = sc.nextInt();
-        System.out.println("Digite quantas colunas a matriz vai possuir:");
+        System.out.println("Digite o número de linhas da matriz: ");
         int m = sc.nextInt();
+        System.out.println("Digite o número de colunas da matriz: ");
+        int n = sc.nextInt();
+        int[][] mat = new int[m][n];
 
-        int[][] mat = new int[n][m];
-
-        for (int i = 0; i < n ; i++){
-            for (int j = 0; j < m ; j++){
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat[i].length; j++) {
                 mat[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Digite um numero que pertence a matriz: ");
+
+        System.out.println("Digite o número do X que está na matriz: ");
         int x = sc.nextInt();
-        int count = 0;
-        for (int i = 0; i < mat.length; i++){
-            for (int j = 0 ; j < mat[i].length; j++){
-                if (mat[i][j] == x){
-                    System.out.println(mat[i][i]);
-                    count++;
+
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat[i].length; j++) {
+                if (mat[i][j] == x) {
+                    System.out.println("Position " + i + "," + j + ":");
+                    if (j > 0) {
+                        System.out.println("Left: " + mat[i][j-1]);
+                    }
+                    if (i > 0) {
+                        System.out.println("Up: " + mat[i-1][j]);
+                    }
+                    if (j < mat[i].length-1) {
+                        System.out.println("Right: " + mat[i][j+1]);
+                    }
+                    if (i < mat.length-1) {
+                        System.out.println("Down: " + mat[i+1][j]);
+                    }
                 }
             }
         }
-        System.out.println("Position count: " + count);
-
 
         sc.close();
+
+
     }
 }
